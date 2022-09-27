ff12aae5e828df394b55d430782e8d0bef0dad71
Deportator <fabrichnov87@gmail.com>
2022-09-27 15:59:23 +0300

    Allure

--- src/test/java/CardDeliveryNewDateTest.java
+++ src/test/java/CardDeliveryNewDateTest.java
@@ -1,9 +1,7 @@
 import com.codeborne.selenide.Condition;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -11,12 +9,15 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
-
-
-public class CardDeliveryNewDateTest {
+class DeliveryTest {
     @BeforeAll
     static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @BeforeEach
@@ -26,14 +27,14 @@ public class CardDeliveryNewDateTest {
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
-    void shouldReplanMeeting() {
+    void shouldSuccessfulPlanAndReplanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $("[placeholder='Город']").setValue(validUser.getCity());
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "A",BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "A", BACK_SPACE);
         $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
         $("[name='name']").setValue(validUser.getName());
         $("[name='phone']").setValue(validUser.getPhone());
@@ -41,13 +42,13 @@ public class CardDeliveryNewDateTest {
         $x("//span[contains(text(), 'Запланировать')]").click();
         $x("//*[contains(text(),'Успешно!')]").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("div .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "A",BACK_SPACE);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "A", BACK_SPACE);
         $("[data-test-id='date'] .input__control").setValue(secondMeetingDate);
         $x("//span[@class='button__text']").click();
         $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $x("//span[contains(text(), 'Перепланировать')]").click();
         $("[data-test-id='success-notification']").shouldHave(Condition.exactText("Успешно! Встреча успешно запланирована на " + secondMeetingDate),
                 Duration.ofSeconds(15));
+
     }
 }
-
